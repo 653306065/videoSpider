98813e14a77398fed3605e6af8e3c4619ef9eb4d
fuqiang <qiang.fu@oceanbluecloud.com>
2020-12-01 18:05:16 +0800

    修改代码

--- src/main/java/com/spider/web/Javhuge.java
+++ src/main/java/com/spider/web/Javhuge.java
@@ -1,17 +1,26 @@
 package com.spider.web;
 
+import com.spider.entity.Video;
+import com.spider.service.AvInfoService;
+import com.spider.service.VideoService;
 import com.spider.utils.FFmpegUtil;
 import com.spider.utils.JsoupUtil;
+import com.spider.utils.MD5Util;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,6 +40,12 @@ public class Javhuge {
     @Value("${filterKey}")
     private List<String> filterKey;
 
+    @Autowired
+    VideoService videoService;
+
+    @Autowired
+    AvInfoService avInfoService;
+
 
     public List<Map<String, String>> getVideoList(String category, Integer page) {
         String url = template.replace("@{category}", category).replace("@{page}", String.valueOf(page));
@@ -42,7 +57,7 @@ public class Javhuge {
             if (element.getElementsByTag("a").size() != 0) {
                 Map<String, String> map = new HashMap<>();
                 String videoUrl = home + element.getElementsByTag("a").get(0).attr("href");
-                String title =element.getElementsByTag("a").get(0).attr("title");
+                String title = element.getElementsByTag("a").get(0).attr("title");
                 map.put("url", videoUrl);
                 map.put("title", title);
                 return map;
@@ -62,21 +77,56 @@ public class Javhuge {
     }
 
     public void download(String category) {
-        int page=1;
-        while (true){
-            List<Map<String, String>> list = getVideoList(category, page);
-            list.stream().parallel().forEach(map -> {
-                String url = map.get("url");
-                String title = map.get("title");
-                String m3u8 = getVideoInfo(url);
-                for(String key:filterKey){
-                  if(title.contains(key)){
-                      logger.info("{},包含过滤字段:{}",title,key);
-                      return;
-                  }
+        int page = 1;
+        while (true) {
+            try {
+                List<Map<String, String>> list = getVideoList(category, page);
+                if(CollectionUtils.isEmpty(list)){
+                    continue;
                 }
-                FFmpegUtil.downloadM3U8(m3u8, savePath + title.trim() + ".mp4");
-            });
+                list.stream().parallel().forEach(map -> {
+                    String url = map.get("url");
+                    String title = map.get("title");
+                    String m3u8 = getVideoInfo(url);
+                    for (String key : filterKey) {
+                        if (title.contains(key)) {
+                            logger.info("{},包含过滤字段:{}", title, key);
+                            return;
+                        }
+                    }
+                    AtomicReference<String> avCode = new AtomicReference<>(null);
+                    AvInfoService.codeTransformMap.entrySet().stream().forEach(entry -> {
+                        entry.getValue().stream().forEach(code -> {
+                            if (title.contains(code)) {
+                                logger.info("{},的code为{}", title, entry.getKey());
+                                avCode.set(entry.getKey());
+                            }
+                        });
+                    });
+
+                    if (Objects.nonNull(avCode.get())) {
+                        Video video = videoService.findOnekeyValue("avCode", avCode.get());
+                        if (Objects.nonNull(video)) {
+                            logger.info("{},的视频已存在,savePath:{}", title, video.getSavePath());
+                            return;
+                        }
+                    }
+                    String path = savePath + title.trim() + ".mp4";
+                    FFmpegUtil.downloadM3U8(m3u8, path);
+                    Video video = new Video();
+                    video.setName(title.trim());
+                    video.setSavePath(path);
+                    video.setSourceUrl(m3u8);
+                    video.setVideoUrl(m3u8);
+                    video.setAvCode(avCode.get());
+                    video.setFormat("mp4");
+                    video.setMd5(MD5Util.md5(new File(path)));
+                    videoService.insert(video);
+                    logger.info("{},保存完成", title);
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             page++;
         }
     }
