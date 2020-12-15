89de2c053cd9199b87d03e68a231835d0e649d59
fuqiang <qiang.fu@oceanbluecloud.com>
2020-12-15 16:06:03 +0800

    增加es

--- src/main/java/com/spider/service/es/BaseElasticsearchsService.java
+++ src/main/java/com/spider/service/es/BaseElasticsearchsService.java
@@ -1,13 +1,22 @@
 package com.spider.service.es;
 
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.document.Document;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.StringQuery;
+import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 
 import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
-import java.util.List;
+import java.util.*;
 
 
 public abstract class BaseElasticsearchsService<T> {
@@ -15,15 +24,53 @@ public abstract class BaseElasticsearchsService<T> {
     private Class<T> clazz;
 
     @PostConstruct
-    public void init() {
+    protected void init() {
         clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 
     @Autowired
     protected ElasticsearchRestTemplate elasticsearchRestTemplate;
 
-    public List<SearchHit<T>> searchByValue(String value) {
-        StringQuery stringQuery = new StringQuery(value);
-        return elasticsearchRestTemplate.search(stringQuery, clazz).toList();
+    public List<SearchHit<T>> searchByKeyValue(String key, String value) {
+        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder().must(new MatchQueryBuilder(key, value));
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(boolQueryBuilder).build();
+        return elasticsearchRestTemplate.search(searchQuery, clazz).toList();
+    }
+
+    public void save(T t) {
+        elasticsearchRestTemplate.save(t);
+    }
+
+    public void saveList(Collection<T> collection) {
+        elasticsearchRestTemplate.save(collection);
+    }
+
+    public void delete(T t) {
+        elasticsearchRestTemplate.delete(t);
+    }
+
+    public void deleteById(String id) {
+        elasticsearchRestTemplate.delete(id, clazz);
+    }
+
+    public void updateById(T t) {
+        try {
+            Field id = clazz.getDeclaredField("id");
+            id.setAccessible(true);
+            String idStr = String.valueOf(id.get(t));
+            Map<String, Object> params = new HashMap<>();
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                if (Objects.nonNull(field.get(t))) {
+                    params.put(field.getName(), field.get(t));
+                }
+            }
+            IndexCoordinates indexCoordinates=elasticsearchRestTemplate.getIndexCoordinatesFor(clazz);
+            UpdateQuery updateQuery= UpdateQuery.builder(idStr).withDocument(Document.from(params)).build();
+            elasticsearchRestTemplate.update(updateQuery,indexCoordinates);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/spider/service/es/EsAvInfoService.java
@@ -0,0 +1,8 @@
+package com.spider.service.es;
+
+import com.spider.entity.es.EsAvInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EsAvInfoService extends BaseElasticsearchsService<EsAvInfo>{
+}
