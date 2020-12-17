93ec28bde85187cdcb1e45315fc5056f3e919ca4
fuqiang <qiang.fu@oceanbluecloud.com>
2020-12-17 17:50:22 +0800

    增加清理视频的接口

--- src/main/java/com/spider/controller/VideoConller.java
+++ src/main/java/com/spider/controller/VideoConller.java
@@ -2,15 +2,17 @@ package com.spider.controller;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.spider.entity.Video;
+import com.spider.service.VideoService;
 import com.spider.service.es.EsVideoService;
 import com.spider.vo.ResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,9 +22,24 @@ public class VideoConller {
     @Autowired
     private EsVideoService esVideoService;
 
+    @Autowired
+    private VideoService videoService;
+
     @GetMapping("/search/{value}")
     public ResponseVo<List<Video>> search(@PathVariable(value = "value", required = true) String value) {
         List<Video> list = esVideoService.searchByValue(value).stream().map(esAvInfoSearchHit -> BeanUtil.toBean(esAvInfoSearchHit.getContent(), Video.class)).collect(Collectors.toList());
         return ResponseVo.succee(list);
+    }
+
+    @GetMapping("/clean/video")
+    public ResponseVo cleanVideo(@RequestParam(required = false, defaultValue = "800") Integer height, @RequestParam(required = false, defaultValue = "800") Integer width) {
+        AtomicLong size = new AtomicLong(0);
+        videoService.findAll().stream().filter(video -> new File(video.getSavePath()).exists()).filter(video -> Objects.nonNull(video.getMultimediaInfo())).parallel().forEach(video -> {
+            if (height * width > video.getMultimediaInfo().getVideo().getSize().getHeight() * video.getMultimediaInfo().getVideo().getSize().getWidth()) {
+                //new File(video.getSavePath()).delete();
+                size.addAndGet(video.getSize());
+            }
+        });
+        return ResponseVo.succee(size);
     }
 }
