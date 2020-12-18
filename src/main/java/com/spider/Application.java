7c089bc25d30967affa22fb653dc206bbfa950fc
fuqiang <qiang.fu@oceanbluecloud.com>
2020-12-18 14:19:30 +0800

    修改代码

--- src/main/java/com/spider/Application.java
+++ src/main/java/com/spider/Application.java
@@ -1,22 +1,12 @@
 package com.spider;
 
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableOpenApi
-@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
-    static Logger logger = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class);
     }
 }
