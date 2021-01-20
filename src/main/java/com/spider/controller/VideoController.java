bad2cb0503a48ae49399194f51342e049db7121c
fuqiang <qiang.fu@oceanbluecloud.com>
2021-01-20 15:26:05 +0800

    增加获取视频截图的接口

--- src/main/java/com/spider/controller/VideoController.java
+++ src/main/java/com/spider/controller/VideoController.java
@@ -4,17 +4,17 @@ import cn.hutool.core.bean.BeanUtil;
 import com.spider.entity.Video;
 import com.spider.service.VideoService;
 import com.spider.service.es.EsVideoService;
+import com.spider.utils.FFmpegUtil;
+import com.spider.utils.FileUtils;
 import com.spider.vo.ResponseVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ws.schild.jave.MultimediaInfo;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -42,7 +42,7 @@ public class VideoController extends BaseController {
     @GetMapping("/clean/video")
     public ResponseVo<Object> cleanVideo(@RequestParam(required = false, defaultValue = "640") Integer height, @RequestParam(required = false, defaultValue = "640") Integer width) {
         AtomicLong size = new AtomicLong(0);
-        CopyOnWriteArrayList<String> copyOnWriteArrayList=new CopyOnWriteArrayList<>();
+        CopyOnWriteArrayList<String> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
         videoService.findAll().stream().filter(video -> new File(video.getSavePath()).exists()).filter(video -> Objects.nonNull(video.getMultimediaInfo())).forEach(video -> {
             if (height * width > video.getMultimediaInfo().getVideo().getSize().getHeight() * video.getMultimediaInfo().getVideo().getSize().getWidth()) {
                 logger.info(video.getSavePath());
@@ -51,10 +51,35 @@ public class VideoController extends BaseController {
                 copyOnWriteArrayList.add(video.getSavePath());
             }
         });
-        Map<String,Object> map=new HashMap<>();
-        map.put("fileSize",size.get() / 1024.0 / 1024 / 1024);
-        map.put("list",copyOnWriteArrayList);
-        map.put("listSize",copyOnWriteArrayList.size());
+        Map<String, Object> map = new HashMap<>();
+        map.put("fileSize", size.get() / 1024.0 / 1024 / 1024);
+        map.put("list", copyOnWriteArrayList);
+        map.put("listSize", copyOnWriteArrayList.size());
         return ResponseVo.succee(map);
+    }
+
+    @ApiOperation("获取视频截图")
+    @GetMapping(value = "/screenshot")
+    public ResponseVo<List<String>> getVideoScreenshot(@RequestParam("videoPath") String videoPath,
+                                                       @RequestParam("screenshotPath") String screenshotPath,
+                                                       @RequestParam("count") Integer count) {
+        MultimediaInfo multimediaInfo = FFmpegUtil.getVideoInfo(new File(videoPath));
+        if (Objects.isNull(multimediaInfo)) {
+            return ResponseVo.failure(-1, "文件非视频");
+        }
+        File file=new File(screenshotPath);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        if(!file.isDirectory()){
+            return ResponseVo.failure(-1, "截图路径不是文件夹");
+        }
+        for (int i = 0; i < count; i++) {
+            long index0 = (long) (multimediaInfo.getDuration() / (10 - i)) / 1000;
+            FFmpegUtil.videoSnapshot(videoPath, screenshotPath, String.valueOf(i), index0, 1);
+        }
+        List<File> fileList = new ArrayList<>();
+        FileUtils.getPathFileList(screenshotPath, fileList);
+        return ResponseVo.succee(fileList.stream().map(File::getAbsolutePath).collect(Collectors.toList()));
     }
 }
