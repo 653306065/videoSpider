2f937ae99057ac95bdb7a44fa65f5fcd9d26270b
fuqiang <653306065@qq.com>
2021-07-23 18:33:01 +0800

    redis bean修改

--- src/main/java/com/spider/conf/RedisConf.java
+++ src/main/java/com/spider/conf/RedisConf.java
@@ -6,18 +6,32 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConf {
-
-    @Bean
+    
     @Primary
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+    @Bean("stringRedisTemplate")
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate template = new StringRedisTemplate(factory);
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         template.setValueSerializer(stringRedisSerializer);
         template.setHashValueSerializer(stringRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean("objectRedisTemplate")
+    public RedisTemplate<Object, Object> objectRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+        template.setKeySerializer(jdkSerializationRedisSerializer);
+        template.setValueSerializer(jdkSerializationRedisSerializer);
+        template.setHashValueSerializer(jdkSerializationRedisSerializer);
+        template.setDefaultSerializer(jdkSerializationRedisSerializer);
+        template.setConnectionFactory(factory);
         template.afterPropertiesSet();
         return template;
     }
