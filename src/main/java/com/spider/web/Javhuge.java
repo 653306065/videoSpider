263cd03277d2783b4d7427ef0c7dc05280d0133e
fuqiang <653306065@qq.com>
2021-08-11 14:05:58 +0800

    修改爬虫

--- src/main/java/com/spider/web/Javhuge.java
+++ src/main/java/com/spider/web/Javhuge.java
@@ -3,9 +3,12 @@ package com.spider.web;
 import com.spider.entity.Video;
 import com.spider.utils.FileUtils;
 import com.spider.utils.JsoupUtil;
+import com.spider.utils.OKHttpUtils;
 import io.lindstrom.m3u8.model.MasterPlaylist;
+import io.lindstrom.m3u8.model.Variant;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -45,9 +48,15 @@ public class Javhuge extends BaseWeb {
             return null;
         }
         Map<String, String> map = new HashMap<>();
-        map.put("name", document.getElementsByClass("active").get(0).text());
-        Element script = document.getElementsByTag("script").stream().filter(element -> element.data().contains("uul")).collect(Collectors.toList()).get(0);
-        map.put("url", script.data().split("'")[1].replace("全集$", "").replace("在线播放$", "").replace("第01集$", "").replace("第1集$", ""));
+        map.put("name", document.getElementsByClass("myui-vodlist__thumb wide  picture").get(0).attr("title"));
+        Elements elements = document.getElementsByClass("btn btn-warm");
+        if (CollectionUtils.isEmpty(elements)) {
+            return null;
+        }
+        String videPlayUrl = home + elements.get(0).attr("href");
+        document = JsoupUtil.getDocument(videPlayUrl, enableProxy);
+        Element script = document.getElementsByTag("script").stream().filter(element -> element.data().contains("ura")).findFirst().get();
+        map.put("url", script.data().split("\"")[1]);
         return map;
     }
 
@@ -55,36 +64,46 @@ public class Javhuge extends BaseWeb {
         int page = 1;
         while (true) {
             try {
-                List<Map<String, String>> list = getVideoList(category, page);
-                if(CollectionUtils.isEmpty(list)){
+                List<Map<String, String>> vidoeList = getVideoList(category, page);
+                if (CollectionUtils.isEmpty(vidoeList)) {
                     continue;
                 }
-                list.stream().sequential().forEach(map -> {
+                vidoeList.stream().sequential().forEach(map -> {
                     String url = map.get("url");
                     String title = FileUtils.repairPath(map.get("title"));
-                    if (hasFilterKey(title)) {
-                        logger.info("{},包含过滤字段", title);
-                        return;
-                    }
                     Video video = new Video();
                     video.setSourceUrl(url);
                     video.setName(title);
                     if (videoExistVerify(video)) {
-                        logger.info("{},已存在", title);
-                        return;
-                    }
-                    Map<String, String> videoInfo = getVideoInfo(url);
-                    String path = savePath + fileSeparator + category + fileSeparator + simpleDateFormat.format(new Date()) + fileSeparator + videoInfo.get("name") + ".mp4";
-                    video.setName(videoInfo.get("name") + ".mp4");
-                    video.setSavePath(path);
-                    String masterM3u8 = videoInfo.get("url");
-                    MasterPlaylist masterPlaylist = hlsDownloader.getMasterPlaylist(masterM3u8, enableProxy);
-                    if (Objects.nonNull(masterPlaylist)) {
-                        String m3u8 = masterPlaylist.variants().get(0).uri();
-                        if (!m3u8.startsWith("http")) {
-                            m3u8 = masterM3u8.substring(0, masterM3u8.lastIndexOf("/") + 1) + m3u8;
+                        Map<String, String> videoInfo = getVideoInfo(url);
+                        String path = savePath + fileSeparator + category + fileSeparator + simpleDateFormat.format(new Date()) + fileSeparator + videoInfo.get("name") + ".mp4";
+                        video.setName(videoInfo.get("name") + ".mp4");
+                        video.setSavePath(path);
+                        String m3u8Url = videoInfo.get("url");
+                        String m3u8Str=  OKHttpUtils.get(m3u8Url,enableProxy);
+                        if(hlsDownloader.isMasterPlaylist(m3u8Str)){
+                            MasterPlaylist masterPlaylist = hlsDownloader.getMasterPlaylist(m3u8Str);
+                            //分辨率排序
+                            List<Variant> list = masterPlaylist.variants().stream().filter(variant -> variant.resolution().isPresent()).
+                                    sorted(Comparator.comparing(variant -> variant.resolution().get().height() * variant.resolution().get().width())).
+                                    collect(Collectors.toList());
+                            Collections.reverse(list);
+                            if (org.apache.commons.collections.CollectionUtils.isEmpty(list)) {
+                                list = masterPlaylist.variants();
+                            }
+                            if (!list.get(0).uri().startsWith("http")) {
+                                if (list.get(0).uri().startsWith("/")) {
+                                    String[] strs = m3u8Url.split("/");
+                                    video.setVideoUrl(strs[0] + "/" + strs[1] + "/" + strs[2] + list.get(0).uri());
+                                } else {
+                                    video.setVideoUrl(m3u8Url.substring(0, m3u8Url.lastIndexOf("/") + 1) + masterPlaylist.variants().get(0).uri());
+                                }
+                            }else{
+                                video.setVideoUrl(list.get(0).uri());
+                            }
+                        }else{
+                            video.setVideoUrl(m3u8Url);
                         }
-                        video.setVideoUrl(m3u8);
                         hlsDownloader.downloadByVideo(video, thread, enableProxy);
                     }
                 });
